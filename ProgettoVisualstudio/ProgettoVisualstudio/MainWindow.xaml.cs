952ae66ca779e659be0b80d892a9741c6a07af26
simoghrd <locatellisimone0821@gmail.com>
2025-12-26 13:36:39 +0100

    Aggiunta pagina di registrazione utente e completato il primo livello
    Ho finito il primo livello aggiungendo che dopo tre click compare il livello successivo e inoltre creato la pagina di registrazione utente gestendo la scrittura file

--- ProgettoVisualstudio/ProgettoVisualstudio/MainWindow.xaml.cs
+++ ProgettoVisualstudio/ProgettoVisualstudio/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProgettoVisualstudio
 {
@@ -20,19 +22,38 @@ namespace ProgettoVisualstudio
     /// </summary>
     public partial class MainWindow : Window
     {
+        int contatoreClick = 0;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void button_start_Click(object sender, RoutedEventArgs e)
-        {
+            livello2.Visibility = Visibility.Hidden;
 
         }
 
         private void Livello2_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void button_start_Click_1(object sender, RoutedEventArgs e)
+        {
+            contatoreClick++;
+
+            if (contatoreClick == 3)
+            {
+                livello2.Visibility = Visibility.Visible;
+            }
+
+            if (contatoreClick  > 3)
+            {
+                contatoreClick = 0;
+            }
+        }
+
+
+        private void ApriRegistrazione_Click(object sender, RoutedEventArgs e)
+        {
+            Registrazione finestra = new Registrazione();
+            finestra.Show();
         }
     }
 }
---
+++ ProgettoVisualstudio/ProgettoVisualstudio/Registrazione.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgettoVisualstudio
+{
+    /// <summary>
+    /// Logica di interazione per Registrazione.xaml
+    /// </summary>
+    public partial class Registrazione : Window
+    {
+        public Registrazione()
+        {
+            InitializeComponent();
+        }
+
+        private void button_salva_Click(object sender, RoutedEventArgs e)
+        {
+            String nomeFile = "Utenti.csv";
+            StreamWriter reader;
+
+            try {
+                reader = new StreamWriter(nomeFile, true);
+
+                reader.WriteLine(txtNomeUtente.Text);
+                MessageBox.Show("Utente salvato con successo");
+                reader.Close();
+            }
+
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("File non trovato");
+
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Errore di I/O");
+            }
+        }
+    }
+}
